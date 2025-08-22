51c50127f217b52c76331ef1bb18899e6832db6a
Harshavardhan <harshagowdaout987@gmail.com>
2025-08-22 12:08:44 +0530

    using only if statement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,30 @@
 fun main() {
+//    println("enter a number :")
+//    val input = readln()
+//    val numberInt = input.toIntOrNull()
+//    if (numberInt != null) {
+//        val output = when {
+//            numberInt % 2 == 0 -> "the number is even"
+//            else -> "the number is odd"
+//        }
+//        println(output)
+//    } else {
+//        println("enter a valid input")
+//    }
+
+ //even Checker with if statements
     println("enter a number :")
     val input = readln()
     val numberInt = input.toIntOrNull()
-    if (numberInt != null) {
-        val output = when {
-            numberInt % 2 == 0 -> "the number is even"
-            else -> "the number is odd"
-        }
-        println(output)
-    } else {
+    if (numberInt == null){
         println("enter a valid input")
+    } else if (numberInt % 2 == 0){
+        println("the number is even")
+    } else {
+        println("the number is odd")
     }
-}
 
+}
 
 
 
